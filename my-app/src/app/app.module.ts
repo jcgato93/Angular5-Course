80ad289ede7c53ea09a76ad841af443dde383b40
jcgato93 <npgcamilo@gmail.com>
2018-08-21 22:52:35 -0500

    Instalacion de librerias con NPM

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,10 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'My key'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
