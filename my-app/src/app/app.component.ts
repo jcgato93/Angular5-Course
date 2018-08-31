68b22337018105316e76d7f2287e1f86ef8fb916
jcgato93 <npgcamilo@gmail.com>
2018-08-30 23:30:59 -0500

    ngSwitch

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  lugares:any  =  [
+    {cercania: 1, distancia: 1, active: true, nombre: 'Floreria la gardenia'},
+    {cercania: 2, distancia: 1.8, active: true, nombre: 'Donas la pasadita'},
+    {cercania: 2, distancia: 5, active: true, nombre: 'Veterinaria Herllitas Felices'},
+    {cercania: 3, distancia: 10, active: true, nombre: 'Hotel la Gracia'},
+    {cercania: 3, distancia: 35, active: true, nombre: 'Zapateria el clavo'},
+  ]
   lat: number = 51.678418;
   lng: number = 7.809007;
 }
