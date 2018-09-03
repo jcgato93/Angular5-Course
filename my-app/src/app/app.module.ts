ae73e1dcf13e209b634a00b3dc77c53da33a1956
jcgato93 <npgcamilo@gmail.com>
2018-09-03 16:17:07 -0500

    Crear Directivas

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
+import { ResaltarDirective } from './directives/resaltar.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    ResaltarDirective
   ],
   imports: [
     BrowserModule,
