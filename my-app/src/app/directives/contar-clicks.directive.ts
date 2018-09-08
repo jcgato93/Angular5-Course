a17afa32e64abdfa5f19bda0a5219dd5d939c2cd
jcgato93 <npgcamilo@gmail.com>
2018-09-08 12:30:16 -0500

    Host Binders

--- my-app/src/app/directives/contar-clicks.directive.ts
+++ my-app/src/app/directives/contar-clicks.directive.ts
@@ -1,14 +1,16 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
-  selector: 'a[contar-clicks]' //el 'a' indica que solo se usara un anchore 
+  selector: 'li[contar-clicks]' //el 'a' indica que solo se usara un anchore 
 })
 export class ContarClicksDirective {
 
   clickN = 0;
+  @HostBinding('style.opacity') opacity: number = .1;
 
   @HostListener('click',['$event.target']) onClick(btn){
     console.log('a',btn,"Número de Clicks:", this.clickN++);
+    this.opacity += .1;
   }
 
   constructor() { }
