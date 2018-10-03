2b4bbb13e6f8bab7e36db9f74c8de83d3abebe1d
jcgato93 <npgcamilo@gmail.com>
2018-10-02 22:33:17 -0500

    Animations

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -18,17 +20,22 @@ import { AngularFireDatabaseModule} from'angularfire2/database';
 import { environment} from'../environments/environment';
 import { CrearComponent } from './crear/crear.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LinkifystrngPipe } from './linkifystrng.pipe';
+import { LinkifystrngPipe } from './pipes/linkifystrng.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,    
     ResaltarDirective, 
     ContarClicksDirective, 
-    DetalleComponent, LugaresComponent, ContactoComponent, CrearComponent, LinkifystrngPipe
+    DetalleComponent, 
+    LugaresComponent, 
+    ContactoComponent, 
+    CrearComponent, 
+    LinkifystrngPipe
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'My key'
--- my-app/src/app/lugares/lugares.component.ts
+++ my-app/src/app/lugares/lugares.component.ts
@@ -1,16 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { LugaresService } from '../services/lugares.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-lugares',
   templateUrl: './lugares.component.html',
-  styleUrls: ['./lugares.component.css']
+  styleUrls: ['./lugares.component.css'],
+  animations: [
+    trigger('nameTrigger',
+    [
+      state('nameState',style({
+        opacity: 0,
+        backgroundColor: 'green',
+        transform: 'rotate3d(0,0,0,0deg)'
+      })),
+      state('nameStateFinal',style({
+        opacity: 1,
+        backgroundColor: 'yellow',
+        transform: 'rotate3d(5,10,30,0deg)'
+      })),
+
+      transition('nameState => nameStateFinal', animate(1000)),
+      transition('nameStateFinal => nameState', animate(500))
+
+    ])
+  ]
 })
 export class LugaresComponent implements OnInit {
   
   ngOnInit(): void {
     
   }
+
+  //Example of animations
+  state = 'nameState';
+
+  animar(){
+    this.state = (this.state === 'nameState') ? 'nameStateFinal' : 'nameState';
+  }
+
 
   title = 'my-app';  
   lat: number = 51.678418;
