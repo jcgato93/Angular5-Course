53e59b2986c9828bd781319f40343b8c22de5e40
Camilo.IT <npgcamilo@gmail.com>
2018-10-15 17:12:05 -0500

    modificacion archivo sobre directives

--- my-app/src/environments/environment.prod.ts
+++ my-app/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyAsda9CWA6tT6dvizpho5bJxndSODIZC_k",
+    authDomain: "angularcourse-ee1b4.firebaseapp.com",
+    databaseURL: "https://angularcourse-ee1b4.firebaseio.com",
+    projectId: "angularcourse-ee1b4",
+    storageBucket: "angularcourse-ee1b4.appspot.com",
+    messagingSenderId: "652698432483"
+  }
+
 };
